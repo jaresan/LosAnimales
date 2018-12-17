7fa7a9fb7e23ec0f53d5764e28e9c4cfb8280027
SaruneUrvikyte <sarune.urvikyte@gmail.com>
2018-12-17 12:10:47 +0000

    Admin page

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -2,8 +2,48 @@ import React from 'react';
 import { Link }from 'react-router-dom';
 
 export default props => {
-  return<div>
-    <Link to="/admin/add-species">Add animal species</Link>
-    <Link to="/admin/add-animal">Add animal</Link>
+  return (
+    <div>
+      <h3>
+          Administrator page
+      </h3>
+      <hr/>
+    <div id="admin-page">
+    <div className="porque">
+      <Link to="/admin/add-species">
+        <button1>
+          <img src="/img/addSpecie.png"/>
+          <h6>
+              Add new species
+          </h6>
+        </button1>
+      </Link>
+      <Link to="/admin/add-animal">
+        <button1>
+          <img src="/img/addAnimal.png"/>
+          <h6>
+              Add new animals
+          </h6>
+        </button1>
+      </Link>
+      <Link to="/admin/edit-animal">
+          <button1>
+              <img src="/img/editSpecie.png"/>
+              <h6>
+                  Edit species
+              </h6>
+          </button1>
+      </Link>
+        <Link to="/admin/edit-animal">
+            <button1>
+                <img src="/img/editAnimal.png"/>
+                <h6>
+                    Edit animal
+                </h6>
+            </button1>
+        </Link>
+    </div>
   </div>
+  </div>
+  )
 }
--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://los-animales-server.herokuapp.com';
+const baseUrl = process.env.NODE_ENV === 'development' ? 'https://los-animales-server.herokuapp.com' : 'https://los-animales-server.herokuapp.com';
 export const API = {
   baseUrl,
   getAll: `${baseUrl}/list`

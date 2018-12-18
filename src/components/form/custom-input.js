96d2c77fc8d8fc4e984e2f313f5033e9924ae618
Jozef Lelic <joseph.lelic@gmail.com>
2018-12-18 00:06:47 +0000

    Add validations to registration form

--- src/components/form/custom-input.js
+++ src/components/form/custom-input.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 
 export  default class extends Component {
   render(){
-    const { input, placeholder, type} = this.props;
+    const { input, placeholder, type, meta: { touched, error, warning }} = this.props;
     return (
+      <div>
         <input
           placeholder={placeholder}
           className="form-control"
           type={type}
           {...input}
         />
+        <div className="error-message">
+          {touched &&
+          ((error && <span>{error}</span>) ||
+          (warning && <span>{warning}</span>))}
+        </div>
+      </div>
     )
   }
 }

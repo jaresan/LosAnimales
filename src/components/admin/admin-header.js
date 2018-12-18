5c249d00f95e4a11ddb420f0452ae2ddf6c00e7a
Jozef Lelic <joseph.lelic@gmail.com>
2018-12-18 01:23:09 +0000

    Fix admin header

--- src/components/admin/admin-header.js
+++ src/components/admin/admin-header.js
@@ -5,10 +5,12 @@ import User from '../user/user';
 export default props => {
   return (
     <div>
-      <Link to="/" className="navbar-brand js-scroll-trigger" href="#page-top ">
-        <img alt='logo' src="/img/home_logo.png" style={{width:'60px'}}/>
-      </Link>
-      <User/>
+      <div id="admin-header">
+        <Link to="/" className="navbar-brand js-scroll-trigger" href="#page-top ">
+          <img alt='logo' src="/img/home_logo.png" style={{width:'60px'}}/>
+        </Link>
+        <User/>
+      </div>
       <h3>
           Administrator page
       </h3>
